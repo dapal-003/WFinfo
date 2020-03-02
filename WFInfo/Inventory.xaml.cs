b8350f65e9ac32f15bba56f76781d2bd56177e40
dapal-003 <Dapal003@gmail.com>
2020-03-02 22:31:31 +0100

    Implementation of some of the code behind.

--- WFInfo/Inventory.xaml.cs
+++ WFInfo/Inventory.xaml.cs
@@ -10,7 +10,7 @@ namespace WFInfo {
 	public partial class Inventory : Window {
 
         private bool searchActive = false;
-        private bool showAllRelics = false;
+        private bool showAllItems = false;
         public static List<TreeNode> InventoryNodes { get; set; }
         public static string[] searchText;
 
@@ -19,124 +19,123 @@ namespace WFInfo {
         }
 
         private void VaultedClick(object sender, RoutedEventArgs e) {
-            //if ((bool)vaulted.IsChecked) {
-            //    foreach (TreeNode era in RelicNodes)
-            //        era.FilterOutVaulted(true);
+            if ((bool)vaulted.IsChecked) {
+                foreach (TreeNode era in InventoryNodes)
+                    era.FilterOutVaulted(true);
 
-            //    RefreshVisibleRelics();
-            //} else
-            //    ReapplyFilters();
+                RefreshVisibleeItems();
+            } else
+                ReapplyFilters();
         }
 
-        private void RefreshVisibleRelics() {
+        private void RefreshVisibleeItems() {
             int index = 0;
-            //if (showAllRelics) {
-            //    List<TreeNode> activeNodes = new List<TreeNode>();
-            //    foreach (TreeNode era in RelicNodes)
-            //        foreach (TreeNode relic in era.ChildrenFiltered)
-            //            activeNodes.Add(relic);
+            if (showAllItems) {
+                List<TreeNode> activeNodes = new List<TreeNode>();
+                foreach (TreeNode item in InventoryNodes)
+                        activeNodes.Add(item);
 
 
-            //    for (index = 0; index < RelicTree.Items.Count;) {
-            //        TreeNode relic = (TreeNode)RelicTree.Items.GetItemAt(index);
-            //        if (!activeNodes.Contains(relic))
-            //            RelicTree.Items.RemoveAt(index);
-            //        else {
-            //            activeNodes.Remove(relic);
-            //            index++;
-            //        }
-            //    }
+                for (index = 0; index < InventoryTree.Items.Count;) {
+                    TreeNode item = (TreeNode)InventoryTree.Items.GetItemAt(index);
+                    if (!activeNodes.Contains(item))
+                        InventoryTree.Items.RemoveAt(index);
+                    else {
+                        activeNodes.Remove(item);
+                        index++;
+                    }
+                }
 
-            //    foreach (TreeNode relic in activeNodes)
-            //        RelicTree.Items.Add(relic);
+                foreach (TreeNode relic in activeNodes)
+                    InventoryTree.Items.Add(relic);
 
-            //    SortBoxChanged(null, null);
-            //} else {
-            //    foreach (TreeNode era in RelicNodes) {
-            //        int curr = RelicTree.Items.IndexOf(era);
-            //        if (era.ChildrenFiltered.Count == 0) {
-            //            if (curr != -1)
-            //                RelicTree.Items.RemoveAt(curr);
-            //        } else {
-            //            if (curr == -1)
-            //                RelicTree.Items.Insert(index, era);
+                SortBoxChanged(null, null);
+            } else {
+                foreach (TreeNode item in InventoryNodes) {
+                    int curr = InventoryTree.Items.IndexOf(item);
+                    if (item.ChildrenFiltered.Count == 0) {
+                        if (curr != -1)
+                            InventoryTree.Items.RemoveAt(curr);
+                    } else {
+                        if (curr == -1)
+                            InventoryTree.Items.Insert(index, item);
 
-            //            index++;
-            //        }
-            //        era.RecolorChildren();
-            //    }
-            //}
-            //RelicTree.Items.Refresh();
+                        index++;
+                    }
+                    item.RecolorChildren();
+                }
+            }
+            InventoryTree.Items.Refresh();
         }
 
         private void ReapplyFilters() {
 
-            //foreach (TreeNode era in RelicNodes)
-            //    era.ResetFilter();
+            foreach (TreeNode item in InventoryNodes)
+                item.ResetFilter();
 
-            //if ((bool)vaulted.IsChecked)
-            //    foreach (TreeNode era in RelicNodes)
-            //        era.FilterOutVaulted(true);
+            if ((bool)vaulted.IsChecked)
+                foreach (TreeNode item in InventoryNodes)
+                    item.FilterOutVaulted(true);
 
-            //if (searchText != null && searchText.Length != 0)
-            //    foreach (TreeNode era in RelicNodes)
-            //        era.FilterSearchText(searchText, false, true);
+            if (searchText != null && searchText.Length != 0)
+                foreach (TreeNode item in InventoryNodes)
+                    item.FilterSearchText(searchText, false, true);
 
-            RefreshVisibleRelics();
+            RefreshVisibleeItems();
         }
 
 
         private void TextboxTextChanged(object sender, TextChangedEventArgs e) {
-            //searchActive = textBox.Text.Length > 0 && textBox.Text != "Filter Terms";
-            //if (textBox.IsLoaded) {
-            //    if (searchActive || (searchText != null && searchText.Length > 0)) {
-            //        if (searchActive)
-            //            searchText = textBox.Text.Split(' ');
-            //        else
-            //            searchText = null;
-            //        ReapplyFilters();
-            //    }
-            //}
+            searchActive = textBox.Text.Length > 0 && textBox.Text != "Filter Terms";
+            if (textBox.IsLoaded) {
+                if (searchActive || (searchText != null && searchText.Length > 0)) {
+                    if (searchActive)
+                        searchText = textBox.Text.Split(' ');
+                    else
+                        searchText = null;
+                    ReapplyFilters();
+                }
+            }
         }
 
         private void SortBoxChanged(object sender, SelectionChangedEventArgs e) {
             // 0 - Name
-            // 1 - Average intact plat
-            // 2 - Average radiant plat
-            // 3 - Difference (radiant-intact)
+            // 1 - Plat
+            // 2 - Ducats
+            // 3 - Ducats to plat
 
-            //if (IsLoaded) {
-            //    foreach (TreeNode era in RelicNodes) {
-            //        era.Sort(SortBox.SelectedIndex);
-            //        era.RecolorChildren();
-            //    }
-            //    if (showAllRelics) {
-            //        RelicTree.Items.SortDescriptions.Clear();
-            //        RelicTree.Items.IsLiveSorting = true;
-            //        switch (SortBox.SelectedIndex) {
-            //            case 1:
-            //            RelicTree.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Intact_Val", System.ComponentModel.ListSortDirection.Descending));
-            //            break;
-            //            case 2:
-            //            RelicTree.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Radiant_Val", System.ComponentModel.ListSortDirection.Descending));
-            //            break;
-            //            case 3:
-            //            RelicTree.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Bonus_Val", System.ComponentModel.ListSortDirection.Descending));
-            //            break;
-            //            default:
-            //            RelicTree.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Name_Sort", System.ComponentModel.ListSortDirection.Ascending));
-            //            break;
-            //        }
-            //        bool i = false;
-            //        foreach (TreeNode relic in RelicTree.Items) {
-            //            i = !i;
-            //            if (i)
-            //                relic.Background_Color = TreeNode.BACK_D_BRUSH;
-            //            else
-            //                relic.Background_Color = TreeNode.BACK_U_BRUSH;
-            //        }
-            //    }
-            //}
+            if (IsLoaded) {
+                foreach (TreeNode era in InventoryNodes) {
+                    era.Sort(SortBox.SelectedIndex);
+                    era.RecolorChildren();
+                }
+                if (showAllItems) {
+                    InventoryTree.Items.SortDescriptions.Clear();
+                    InventoryTree.Items.IsLiveSorting = true;
+                    switch (SortBox.SelectedIndex) {
+                        case 1:
+                        InventoryTree.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Plat_Val", System.ComponentModel.ListSortDirection.Descending));
+                        break;
+                        case 2:
+                        InventoryTree.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Ducat_Val", System.ComponentModel.ListSortDirection.Descending));
+                        break;
+                        case 3:
+                        InventoryTree.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Ratio_Val", System.ComponentModel.ListSortDirection.Descending));
+                        break;
+                        default:
+                        InventoryTree.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Name_Sort", System.ComponentModel.ListSortDirection.Ascending));
+                        break;
+                    }
+                    bool i = false;
+                    foreach (TreeNode relic in InventoryTree.Items) {
+                        i = !i;
+                        if (i)
+                            relic.Background_Color = TreeNode.BACK_D_BRUSH;
+                        else
+                            relic.Background_Color = TreeNode.BACK_U_BRUSH;
+                    }
+                }
+            }
         }
 
         private void TextBoxFocus(object sender, RoutedEventArgs e) {
@@ -147,42 +146,6 @@ namespace WFInfo {
         private void TextBoxLostFocus(object sender, RoutedEventArgs e) {
             if (!searchActive)
                 textBox.Text = "Filter Terms";
-        }
-
-        private void ToggleShowAllRelics(object sender, RoutedEventArgs e) {
-            //showAllRelics = !showAllRelics;
-            //foreach (TreeNode era in RelicNodes)
-            //    foreach (TreeNode relic in era.Children)
-            //        relic.topLevel = showAllRelics;
-
-            //if (showAllRelics)
-            //    relicComboButton.Content = "All Relics";
-            //else
-            //    relicComboButton.Content = "Relic Eras";
-
-
-            //RelicTree.Items.Clear();
-            //RefreshVisibleRelics();
-        }
-
-        private void ExpandAll(object sender, RoutedEventArgs e) {
-            //foreach (TreeNode era in RelicNodes)
-            //    era.ChangeExpandedTo(true);
-        }
-
-        private void CollapseAll(object sender, RoutedEventArgs e) {
-            //foreach (TreeNode era in RelicNodes)
-            //    era.ChangeExpandedTo(false);
-        }
-
-        private void SingleClickExpand(object sender, RoutedEventArgs e) {
-            TreeViewItem tvi = e.OriginalSource as TreeViewItem;
-
-            if (tvi == null || e.Handled) return;
-
-            tvi.IsExpanded = !tvi.IsExpanded;
-            tvi.IsSelected = false;
-            e.Handled = true;
         }
 
         #region Boring basic window stuff
